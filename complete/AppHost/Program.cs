948f407866bfa18edd741b2a88f57195b30e0a99
Ayush Bohra <codefingers809@gmail.com>
2024-12-13 23:21:54 +0530

    complete

---
+++ complete/AppHost/Program.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache")
+	.WithRedisCommander();
+
+var api = builder.AddProject<Projects.Api>("api")
+	.WithReference(cache);
+
+var web = builder.AddProject<Projects.MyWeatherHub>("myweatherhub")
+	.WithReference(api)
+	.WithExternalHttpEndpoints();
+
+
+builder.Build().Run();
